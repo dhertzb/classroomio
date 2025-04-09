ddc018f08374f32e90e5a13e4fea6b18fe86cfad
dhertzb <dhertzb@gmail.com>
2025-04-09 18:44:42 -0300

    feat: show org menu by admin role

--- apps/dashboard/src/lib/components/Org/ProfileMenu/Menu.svelte
+++ apps/dashboard/src/lib/components/Org/ProfileMenu/Menu.svelte
@@ -1,23 +1,21 @@
 <script>
+  import { goto } from '$app/navigation';
+  import { supabase } from '$lib/utils/functions/supabase';
+  import { t } from '$lib/utils/functions/translations';
+  import { capturePosthogEvent } from '$lib/utils/services/posthog';
   import { globalStore } from '$lib/utils/store/app';
-  import { user, profile, defaultProfileState, defaultUserState } from '$lib/utils/store/user';
   import {
     currentOrg,
-    orgs,
-    defaultCurrentOrgState,
     currentOrgDomain,
-    currentOrgPath
+    currentOrgPath,
+    defaultCurrentOrgState,
+    orgs
   } from '$lib/utils/store/org';
-  import { ChevronDown, Settings } from 'carbon-icons-svelte';
+  import { defaultProfileState, defaultUserState, profile, user } from '$lib/utils/store/user';
+  import { Settings } from 'carbon-icons-svelte';
   import Logout from 'carbon-icons-svelte/lib/Logout.svelte';
-  import Rocket from 'carbon-icons-svelte/lib/Rocket.svelte';
-  import NewTab from 'carbon-icons-svelte/lib/NewTab.svelte';
-  import { goto } from '$app/navigation';
-  import { profileMenu } from '../store';
-  import { supabase } from '$lib/utils/functions/supabase';
-  import { capturePosthogEvent } from '$lib/utils/services/posthog';
-  import { t } from '$lib/utils/functions/translations';
   import posthog from 'posthog-js';
+  import { profileMenu } from '../store';
 
   async function logout() {
     const { error } = await supabase.auth.signOut();
@@ -44,19 +42,19 @@
   }
 </script>
 
-<div class="md:px-4 md:py-4 py-2 px-2 rounded-md cursor-pointer">
-  <div class="border-b py-3 space-y-4">
+<div class="cursor-pointer rounded-md px-2 py-2 md:px-4 md:py-4">
+  <div class="space-y-4 border-b py-3">
     <p class="text-xs font-semibold text-gray-500">{$t('profileMenu.profile')}</p>
     <a
       href={`${!$globalStore.isOrgSite ? $currentOrgPath : '/lms'}/settings`}
       class="flex items-center justify-between hover:no-underline"
       on:click={closeMenu}
     >
-      <span class="flex items-center gap-2 max-w-[70%]">
+      <span class="flex max-w-[70%] items-center gap-2">
         <img src={$profile.avatar_url} alt="profile" class="h-8 w-8 rounded-full" />
         <div>
-          <p class="text-sm font-semibold w-[80%] truncate">{$profile.fullname}</p>
-          <p class="text-xs w-[80%] truncate">{$profile.email}</p>
+          <p class="w-[80%] truncate text-sm font-semibold">{$profile.fullname}</p>
+          <p class="w-[80%] truncate text-xs">{$profile.email}</p>
         </div>
       </span>
       <div>
@@ -64,23 +62,23 @@
       </div>
     </a>
   </div>
-  {#if !$globalStore.isOrgSite}
-    <div class="border-b py-3 space-y-4">
+  {#if $profile.role === 'admin'}
+    <div class="space-y-4 border-b py-3">
       <p class="text-xs font-semibold text-gray-500">{$t('profileMenu.current_org')}</p>
       <a
         href={`${$currentOrgPath}/settings?tab=org`}
         class="flex items-center justify-between hover:no-underline"
         on:click={closeMenu}
       >
-        <span class="flex items-center gap-2 max-w-[70%]">
+        <span class="flex max-w-[70%] items-center gap-2">
           <div
-            class="flex items-center justify-center h-8 w-8 p-4 bg-blue-900 text-white font-semibold rounded-lg"
+            class="flex h-8 w-8 items-center justify-center rounded-lg bg-blue-900 p-4 font-semibold text-white"
           >
             {$currentOrg.shortName}
           </div>
           <div>
-            <p class="text-sm font-semibold w-[80%] truncate">{$currentOrg.name}</p>
-            <p class="text-xs w-[80%] truncate">
+            <p class="w-[80%] truncate text-sm font-semibold">{$currentOrg.name}</p>
+            <p class="w-[80%] truncate text-xs">
               {$currentOrgDomain}
             </p>
           </div>
@@ -92,8 +90,7 @@
     </div>
   {/if}
 
-
-  <button on:click={logout} class="w-full pt-3 space-y-4">
+  <button on:click={logout} class="w-full space-y-4 pt-3">
     <span class="flex items-center gap-2">
       <Logout />
       <p class="text-sm font-semibold">{$t('settings.profile.logout')}</p>
